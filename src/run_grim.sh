39e31aba7775bb96cd5cc0a205889d1f4434f1b8
boychaboy <hoon2j@gmail.com>
2021-05-09 13:36:05 +0900

    2000 experiment

--- src/run_grim.sh
+++ src/run_grim.sh
@@ -2,8 +2,7 @@ GPU_ID=$1
 SEED=1
 SPLIT=0.005
 TEST_LEN=2000
-
-MODEL=bert-base-cased
+MODEL=distilroberta-base
 
 CUDA_VISIBLE_DEVICES=$GPU_ID python grim.py \
     --seed $SEED \
@@ -11,7 +10,7 @@ CUDA_VISIBLE_DEVICES=$GPU_ID python grim.py \
     --split_ratio $SPLIT \
     --subtype_len $TEST_LEN \
     --model_name boychaboy/mnli_${MODEL} \
-    --template_A templates/template_A_${TEST_LEN}_${MODEL}.csv \
-    --template_B templates/template_B_${TEST_LEN}_${MODEL}.csv \
-    --template_C templates/template_C_${TEST_LEN}_${MODEL}.csv \
-    --save_dir ../result/${MODEL}_${SPLIT}_${SEED}
+    --template_A ../templates/template_A_${TEST_LEN}_${MODEL}.csv \
+    --template_B ../templates/template_B_${TEST_LEN}_${MODEL}.csv \
+    --template_C ../templates/template_C_${TEST_LEN}_${MODEL}.csv \
+    --save_dir ../result/${MODEL}_${TEST_LEN}_${SEED}
